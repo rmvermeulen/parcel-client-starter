bde82fa19e75e2412c70b0a83e4ecbd59f2a6eaa
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2019-01-27 16:38:31 +0100

    Example app

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  setupTestFrameworkScriptFile: './setupJest.ts',
   verbose: true,
   collectCoverage: true,
   coveragePathIgnorePatterns: ['setupJest'],
